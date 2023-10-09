a7d6adf376998240020232aaf7b97095095a1896
Camilo <camilo@promusica.co>
2023-10-09 23:23:57 +0200

    End of config Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
+let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
+const tailwind = require('tailwindcss')(tailwindConfig);
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    postcss-import: {},
-  },
-}
+	// eslint-disable-next-line no-process-env
+	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+};
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//     postcss-import: {},
+//   },
+// }
