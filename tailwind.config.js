0e06921024bcd790b8af81f24561b2eeb4ba8020
Camilo <camilo@promusica.co>
2023-10-10 16:12:38 +0200

    Config now working: typography and hugo_stats

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const typography = require('@tailwindcss/typography');
+
 module.exports = {
   content: [
     "./layouts/**/*.html",
     "./layouts/_default/**/*.html",
     "./layouts/partials/**/*.{html,js}",
+    "./content/_index.md",
     "./content/**/*.md",
     "./layout/index.html",
     "./public/index.html",
+    "./hugo_stats.json",
 
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
 }
 
